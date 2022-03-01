d46ea35f3a877b9db5eb57e110306aaed8e2bf1a
goldstar611 <goldstar611@users.noreply.github.com>
2022-03-01 16:13:33 -0600

    Initial working recipe.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
+
+## Pre built binaries
+I would love to get feedback on these AppImages. If you aren't able to build from scratch you can still help test using the prebuilt binaries.
+
+https://github.com/goldstar611/chirp-appimage/releases/tag/02Mar2022
+
+- `aarch64.AppImage` for Pi4B, Pi3B & Pi3B+, and Pi Zero W2, running 64-bit OS
+- `armhf.AppImage` for any Pi running a 32-bit OS
+- `x86-64.AppImage` for Intel/AMD running any 64-bit OS
+- `i686.AppImage` for Intel/AMD running any 32-bit OS
+
+
 # chirp-appimage
-Appimage Recipe for Chirp
+AppImage Recipe for Chirp
+
+This recipe builds a Chirp AppImage for the following architectures
+- amd64 (AMD/Intel 64-bit)
+- aarch64 (ARM 64-bit like raspberry pi 4, newer pi-zero)
+- armhf (ARM 32-bit like raspberry pi 4/3/2/1, older pi-zero)
+
+From the [tutorial](https://appimage-builder.readthedocs.io/en/latest/intro/install.html), 
+you can get the build tools by running the following commands in a command prompt on Debian/Ubuntu/LinuxMint:
+```
+# Install build dependencies
+sudo apt install git python2.7-minimal python3-pip python3-setuptools patchelf desktop-file-utils libgdk-pixbuf2.0-dev fakeroot strace fuse
+
+# Install appimagetool
+arch=$(uname -m)
+if [ "${arch}" = "armv7l" ]; then arch="armhf"; fi  # Raspberry pi fix
+sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-${arch}.AppImage -O /usr/local/bin/appimagetool
+sudo chmod +x /usr/local/bin/appimagetool
+
+# Install the appiamge-builder tool
+sudo -H pip3 install appimage-builder
+```
+
+Building an AppImage is super easy if all the dependencies are still available in \*.deb format (like an old ubuntu archive).
+
+Just run 
+```
+git clone https://github.com/goldstar611/chirp-appimage
+cd chirp-appimage/
+chmod +x ./build.sh
+./build.sh
+```
+
+You will find `Chirp-daily-*-*.AppImage` in your directory after the command finishes.
+
+## Using a proxy
+If you would like to use a proxy or a apt caching utility (like apt-cacher-ng) you can set the `http_proxy` environment variable before running the build script.
+```
+export http_proxy=http://localhost:3142
+./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -xe
+
+rm -f Chirp-*-*.AppImage
+
+git clone --depth=1 https://github.com/kk7ds/chirp | true
+cd chirp
+  git pull
+  LATEST_COMMIT_DATE=$(git log -1 --date=format:"%Y%m%d" --format="%ad")
+  LATEST_COMMIT_SHORT=$(git rev-parse --short HEAD)
+  export CHIRP_VERSION="daily-${LATEST_COMMIT_DATE}+${LATEST_COMMIT_SHORT}"
+  # Update in-app version
+  sed -i "s/CHIRP_VERSION = \".*\"/CHIRP_VERSION = \"${CHIRP_VERSION}\"/" chirp/__init__.py
+  # No need to import chirp or chirp.drivers on a linux build
+  sed -i "s/^from chirp.drivers import .*$//" setup.py
+  sed -i "s/^import chirp$//" setup.py
+cd ..
+
+# i386 (32-bit Intel/AMD)
+export TARGET_ARCH_APT=i386
+export TARGET_ARCH_APPIMAGE=i686
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu/ bionic main"
+export SOURCE_LINE_2="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu/ xenial main"
+export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3b4fe6acc0b21f32"
+export KEY_URL_2="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x40976EAF437D05B5"
+appimage-builder --recipe AppImageBuilder.yml
+
+# x86_64 (64-bit Intel/AMD)
+export TARGET_ARCH_APT=amd64
+export TARGET_ARCH_APPIMAGE=x86_64
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu/ bionic main"
+export SOURCE_LINE_2="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu/ xenial main"
+export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3b4fe6acc0b21f32"
+export KEY_URL_2="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x40976EAF437D05B5"
+appimage-builder --recipe AppImageBuilder.yml
+
+# armhf (32-bit ARM)
+export TARGET_ARCH_APT=armhf
+export TARGET_ARCH_APPIMAGE=armhf
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports bionic main"
+export SOURCE_LINE_2="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports xenial main"
+export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3b4fe6acc0b21f32"
+export KEY_URL_2="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x40976EAF437D05B5"
+appimage-builder --recipe AppImageBuilder.yml
+
+# arm64/aarch64 (64-bit ARM)
+export TARGET_ARCH_APT=arm64
+export TARGET_ARCH_APPIMAGE=aarch64
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports bionic main"
+export SOURCE_LINE_2="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports xenial main"
+export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3b4fe6acc0b21f32"
+export KEY_URL_2="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x40976EAF437D05B5"
+appimage-builder --recipe AppImageBuilder.yml
+
