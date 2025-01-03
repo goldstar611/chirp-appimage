50c56525b32d6a23c7a08a5da236d97afc8aa0ba
goldstar611 <goldstar611@users.noreply.github.com>
2025-01-02 21:54:18 -0600

    Use Ubuntu Jammy (22.04) and use published wheel
    20.04 comes with python 3.8 which is not supported by CHIRP
    Using the wheel removes a lot of maintanence overhead (dependencies and version editing_

--- build.sh
+++ build.sh
@@ -3,36 +3,29 @@ set -xe
 
 rm -f Chirp-*-*.AppImage
 
-git clone --depth=1 https://github.com/kk7ds/chirp
-
-cd chirp
-  git pull
-  export CHIRP_VERSION="next-$(date +%Y%m%d)"
-  # Update in-app version
-  sed -i "s/CHIRP_VERSION = \".*\"/CHIRP_VERSION = \"${CHIRP_VERSION}\"/" chirp/__init__.py
-  # No need to import chirp or chirp.drivers on a linux build
-  sed -i "s/^from chirp.drivers import .*$//" setup.py
-  sed -i "s/^import chirp$//" setup.py
-cd ..
+ver_with_next=$(basename $(curl -H "User-Agent: goldstar611" -Ls -o/dev/null -w '%{url_effective}' https://archive.chirpmyradio.com/download?stream=next))
+ver_without_next=${ver_with_next//next-/}
+export CHIRP_VERSION=${ver_with_next}
+curl -H "User-Agent: goldstar611" -o chirp-${ver_without_next}-py3-none-any.whl https://archive.chirpmyradio.com/chirp_next/${ver_with_next}/chirp-${ver_without_next}-py3-none-any.whl
 
 # x86_64 (64-bit Intel/AMD)
 export TARGET_ARCH_APT=amd64
 export TARGET_ARCH_APPIMAGE=x86_64
-export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu focal main universe"
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://archive.ubuntu.com/ubuntu jammy main universe"
 export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x871920D1991BC93C"
 appimage-builder --recipe AppImageBuilder.yml
 
 # armhf (32-bit ARM)
 export TARGET_ARCH_APT=armhf
 export TARGET_ARCH_APPIMAGE=armhf
-export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports focal main universe"
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports jammy main universe"
 export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x871920D1991BC93C"
 appimage-builder --recipe AppImageBuilder.yml
 
 # arm64/aarch64 (64-bit ARM)
 export TARGET_ARCH_APT=arm64
 export TARGET_ARCH_APPIMAGE=aarch64
-export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports focal main universe"
+export SOURCE_LINE_1="deb [arch=${TARGET_ARCH_APT}] http://ports.ubuntu.com/ubuntu-ports jammy main universe"
 export KEY_URL_1="http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x871920D1991BC93C"
 appimage-builder --recipe AppImageBuilder.yml
 
