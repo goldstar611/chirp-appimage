2a36a12661bddbbe0ce79f8aa08a87ff2e06104e
goldstar611 <goldstar611@hotmail.com>
2025-11-10 17:03:19 -0600

    Simplify curl command in build.sh
    Removed User-Agent header from curl command to fetch version.

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -xe
 
 rm -f Chirp-*-*.AppImage
 
-ver_with_next=$(basename $(curl -H "User-Agent: goldstar611" -Ls -o/dev/null -w '%{url_effective}' "https://archive.chirpmyradio.com/download?stream=next"))
+ver_with_next=$(basename $(curl -Ls -o/dev/null -w '%{url_effective}' "https://archive.chirpmyradio.com/download?stream=next"))
 ver_without_next=${ver_with_next//next-/}
 export CHIRP_VERSION=${ver_with_next}
-curl -H "User-Agent: goldstar611" -o chirp-${ver_without_next}-py3-none-any.whl https://archive.chirpmyradio.com/chirp_next/${ver_with_next}/chirp-${ver_without_next}-py3-none-any.whl
+curl -o chirp-${ver_without_next}-py3-none-any.whl https://archive.chirpmyradio.com/chirp_next/${ver_with_next}/chirp-${ver_without_next}-py3-none-any.whl
 
 # x86_64 (64-bit Intel/AMD)
 export TARGET_ARCH_APT=amd64
